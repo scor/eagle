b1e047185a3a2570642633f8039c01625ebc10f5
scor <scorlosquet@gmail.com>
2011-05-27 16:07:28 -0400

    use better argument management using getopts and fix call to CutyCapt

--- eagle-crawl.sh
+++ eagle-crawl.sh
@@ -1,11 +1,59 @@
-die () {
-    echo >&2 "$@"
-    exit 1
+#!/bin/bash
+
+usage()
+{
+cat << EOF
+usage: $0 options
+
+This script crawls a given list of urls from the site given in the -u argument
+
+OPTIONS:
+   -h      Show this message
+   -u      URL of the frontpage to crawl 
+   -v      Verbose
+EOF
 }
 
-[ "$#" -eq 1 ] || die "First argument required and must correspond to the base URL of the site to crawl (must end with /)"
+BASE=
+VERBOSE=
 
-BASE=$1
+# allow for default local configuration
+if [ -f eagleconfig ]; then
+    echo "Using local eagleconfig file"
+    . eagleconfig
+fi
+
+
+# arguments followed by : take a value
+while getopts “hu:v” OPTION
+do
+     case $OPTION in
+         h)
+             usage
+             exit 1
+             ;;
+         u)
+             BASE=$OPTARG
+             ;;
+         v)
+             VERBOSE=1
+             ;;
+         ?)
+             usage
+             exit
+             ;;
+     esac
+done
+
+# required values
+#if [[ -z $TEST ]] || [[ -z $SERVER ]] || [[ -z $PASSWD ]]
+if [[ -z $BASE ]]
+then
+     usage
+     exit 1
+fi
+
+# this is the directory of the baseline screenshots.
 mkdir -p actual
 
 for URL in `cat urls.txt`
@@ -14,8 +62,8 @@ do
   if [ "$URL" = "<front>" ]; then
     URL=''
   fi
-  echo "Processing $BASE$URL"
-  ./CutyCapt --url=$BASE$URL --out=actual/$FILE.png
+  echo "Taking screenshot of $BASE$URL"
+  CutyCapt --url=$BASE$URL --out=actual/$FILE.png
   #compare -metric ae old/$img new/$img diff/$img
 done
 
