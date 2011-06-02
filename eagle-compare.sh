720ceebc393f65bf5c9e97f2156fc1829b40ff43
scor <scorlosquet@gmail.com>
2011-06-02 15:38:50 -0400

    lots of clean up compare script, added -v version parameter

--- eagle-compare.sh
+++ eagle-compare.sh
@@ -1,10 +1,77 @@
-# prepare the diff directory and make sure it is empty
-mkdir -p diff
-rm -r diff/*
+#!/bin/bash
 
-for img in `cd base; ls *.png`
+usage()
+{
+cat << EOF
+usage: $0 options
+
+This script compares a given list of screenshots based on the list of urls.
+
+OPTIONS:
+   -h      Show this message
+   -v      Version of the site currently being tested, whose screenshots will go into the actual directory (this value is only used to add a label to the diff images).
+EOF
+}
+
+VERSION=
+
+# arguments followed by : take a value
+while getopts “hu:v” OPTION
 do
-  echo "Comparing $img"
-  compare -metric ae base/$img actual/$img diff/$img
+     case $OPTION in
+         h)
+             usage
+             exit 1
+             ;;
+         v)
+             VERSION="-$OPTARG"
+             ;;
+         ?)
+             usage
+             exit
+             ;;
+     esac
 done
 
+# prepare the diff directory and make sure it is empty
+mkdir -p diff
+rm -rf diff/*
+
+# Get current version of the repository checkout (only 6 chars to keep it short).
+#COMMIT=`git show`
+#VERSION=${COMMIT:7:6}
+
+
+#for img in `cd base; ls *.png`
+for URL in `cat urls.txt`
+do
+  NAME=$URL
+
+  # Replace slashes in path to fit in a filename.
+  FILE=${URL/\//|}
+
+  # frontpage syntax
+  if [ "$URL" = "<front>" ]; then
+    URL=''
+  fi
+
+  echo "Comparing screenshots for $NAME"
+
+  #First compare the geometry/size of the images.
+  if [ `identify -format %g base/$FILE.png` = `identify -format %g actual/$FILE.png` ]; then
+    # Calculate the absolute error between the two images
+    AE=`compare -metric ae base/$FILE.png actual/$FILE.png diff/$FILE$VERSION.png 2>&1`
+
+    # Delete the diff file if there are no differences.
+    if [ "$AE" = 0 ]; then
+      echo "No difference found for $NAME"
+      rm diff/$FILE.png
+    fi
+
+  fi
+
+done
+
+# the convert command can create a flicker image which works for images which
+# are of different sizes
+# give meaningful name to files so we know what version they are.
